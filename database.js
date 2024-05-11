5047b86a35cca44855a6828d50455c729bc81724
KJ-MU <gullssea5@gmail.com>
2024-05-11 20:55:15 +0300

    add pic to the user profile

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connectedLink = await mongoose.connect(process.env.DB_LINK);
+    const connectedLink = await mongoose.connect(process.env.DB_KEY);
     console.log(`The db is connected: ${connectedLink.connection.host}`);
   } catch (error) {
     console.log("🚀 ~ connectDB ~ error:", error);
